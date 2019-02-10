ca577d30ac78272ee6f29788da96f5d2f3be5667
krzysiek <janekjanecki14@gmail.com>
2019-02-11 00:35:18 +0100

    Add class table mapping

--- src/main/java/com/agh/jbloom/components/mapping/ClassTableMappingService.java
+++ src/main/java/com/agh/jbloom/components/mapping/ClassTableMappingService.java
@@ -1,14 +1,41 @@
 package com.agh.jbloom.components.mapping;
 
+import com.agh.jbloom.components.query.SqlTypeConverter;
+
 public class ClassTableMappingService implements MappingService {
+
+    SqlTypeConverter typeConverter;
+
+    public ClassTableMappingService(SqlTypeConverter typeConverter) {
+        this.typeConverter = typeConverter;
+    }
 
     @Override
     public BaseMapperHandler createMapping(BaseMapperHandler handler, Class c) {
-        return null;
+        BaseMapperHandler newHandler = createMapping(c, handler.getSubject());
+        newHandler.setParent(handler);
+        return newHandler;
     }
 
     @Override
     public BaseMapperHandler createMapping(Class c, Class stop) {
-        return null;
+        String tableName = getTableName(c);
+
+        MapperBuilder handlerBuilder =
+                new SimpleMapperBuilder(c, tableName, typeConverter);
+
+        BaseMapperHandler baseMapperHandler =
+                new BaseMapperHandler(c, handlerBuilder.withClass(c).build());
+        Class current = c;
+
+        while ((current = current.getSuperclass()) != stop){
+            InheritanceMapper mapper = handlerBuilder
+                    .withName(getTableName(current))
+                    .withSubjectClass(current)
+                    .withClass(current)
+                    .build();
+            baseMapperHandler = new BaseMapperHandler(current, baseMapperHandler, mapper);
+        }
+        return baseMapperHandler;
     }
 }
